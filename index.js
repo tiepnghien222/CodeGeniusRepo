101efd0328708b9b764cda3756c02a324cb4783b
tiepnghien222 <dasdsad333pun0suy@hotmail.com>
2024-05-13 19:10:18 +0700

    feat project commit

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-function levelOrder(root) {
-  if (!root) return [];
-  const result = [];
-  const queue = [root];
-  while (queue.length) {
-    const size = queue.length;
-    const level = [];
-    for (let i = 0; i < size; i++) {
-      const node = queue.shift();
-      level.push(node.val);
-      if (node.left) queue.push(node.left);
-      if (node.right) queue.push(node.right);
-    }
-    result.push(level);
-  }
-  return result;
-}
+const numbers = [1, 5, 2, 8];
+const max = Math.max(...numbers);
+console.log(max); // 8
