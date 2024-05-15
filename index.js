953c90764cae963c9bda2560ae5e2ec1ab02ff08
tiepnghien222 <dasdsad333pun0suy@hotmail.com>
2024-05-16 06:43:09 +0700

    feat project

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-const quickSortRandomPivot = (arr) => {
-  if (arr.length <= 1) {
-    return arr;
+function findSubstring(s, words) {
+  if (s.length === 0 || words.length === 0) return [];
+  const wordMap = new Map();
+  for (const word of words) {
+    wordMap.set(word, (wordMap.get(word) || 0) + 1);
   }
-  const pivotIndex = Math.floor(Math.random() * arr.length);
-  const pivot = arr[pivotIndex];
-  const left = [];
-  const right = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (i !== pivotIndex) {
-      if (arr[i] <= pivot) {
-        left.push(arr[i]);
-      } else {
-        right.push(arr[i]);
-      }
+  const wordLength = words[0].length;
+  const totalLength = words.length * wordLength;
+  const result = [];
+  for (let i = 0; i <= s.length - totalLength; i++) {
+    const substring = s.substring(i, i + totalLength);
+    if (isValid(substring, new Map(wordMap))) {
+      result.push(i);
     }
   }
-  return quickSortRandomPivot(left).concat(pivot, quickSortRandomPivot(right));
-};
+  return result;
+  function isValid(substring, wordMap) {
+    const wordCount = new Map();
+    for (let i = 0; i < substring.length; i += wordLength) {
+      const word = substring.substring(i, i + wordLength);
+      wordCount.set(word, (wordCount.get(word) || 0) + 1);
+    }
+    for (const [key, value] of wordMap) {
+      if ((wordCount.get(key) || 0) !== value) return false;
+    }
+    return true;
+  }
+}
